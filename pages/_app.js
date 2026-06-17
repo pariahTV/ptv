10ce9177814569b38cfe5d97c08390ae8de3c1c7
pariahTV <pariahfm@gmail.com>
2026-06-18 00:05:22 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header className="ptv-header">
+        <img
+          src="/logos/pariah-tv-logo.PNG"
+          alt="PariahTV Logo"
+          className="ptv-header-logo"
+        />
+      </header>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
