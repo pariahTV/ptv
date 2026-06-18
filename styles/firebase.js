50fa36e673c53d683b4bbd15637a1d225bc945ec
pariahTV <pariahfm@gmail.com>
2026-06-18 21:04:39 +0100

    Update firebase.js

--- styles/firebase.js
+++ styles/firebase.js
@@ -1,15 +1,14 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCVPVTPSz96AK2DOxxS7ahPUNx7PcNwk4o",
+  authDomain: "ptv1-d75e9.firebaseapp.com",
+  databaseURL: "https://ptv1-d75e9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ptv1-d75e9",
+  storageBucket: "ptv1-d75e9.appspot.com",
+  messagingSenderId: "787477365147",
+  appId: "1:787477365147:web:12928bd7cd088415b8a13e"
 };
 
 export const app = initializeApp(firebaseConfig);
