94aedc286950c9b86090634126f4058b6b8b62f9
pariahTV <pariahfm@gmail.com>
2026-06-18 23:07:12 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
-{
-  "spotlight": {
-    "title": "PariahTV Live",
-    "artist": "Cee Fiitz",
-    "status": "LIVE"
-  },
-  "player": {
-    "nowPlaying": "Intro Set",
-    "duration": "03:48"
-  },
-  "ticker": {
-    "text": "Welcome to PariahTV"
-  }
-}
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVPVTPSz96AK2DOxxS7ahPUNx7PcNwk4o",
+  authDomain: "ptv1-d75e9.firebaseapp.com",
+  databaseURL: "https://ptv1-d75e9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ptv1-d75e9",
+  storageBucket: "ptv1-d75e9.appspot.com",
+  messagingSenderId: "787477365147",
+  appId: "1:787477365147:web:40e2188534d8fca4b8a13e"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
