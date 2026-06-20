3710e6738be3905fd9cb74ea632ed8ae060239c0
pariahTV <pariahfm@gmail.com>
2026-06-20 14:06:04 +0100

    Update ticker.js

--- components/ticker.js
+++ components/ticker.js
@@ -1,7 +1,6 @@
-// components/Ticker.js
 export default function Ticker({ ticker }) {
   return (
-    <section className="ticker">
+    <section className="ticker glass-panel">
       <div className="ticker-text">
         {ticker?.text || "Loading..."}
       </div>
